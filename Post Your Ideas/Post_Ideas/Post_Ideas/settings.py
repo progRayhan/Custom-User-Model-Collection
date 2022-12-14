ef5a8f49e8ae36ed39c2f21a2281b7f4d1bb5a18
Rayhan <prog.rayhan1@gmail.com>
2022-12-14 15:57:22 +0600

    JWT Authentication added

--- Post Your Ideas/Post_Ideas/Post_Ideas/settings.py
+++ Post Your Ideas/Post_Ideas/Post_Ideas/settings.py
@@ -126,3 +126,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.Profile'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ROTATE_REFRESH_TOKENS': True,
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
+}
--- Post Your Ideas/Post_Ideas/accounts/api/urls.py
+++ Post Your Ideas/Post_Ideas/accounts/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from accounts.api.views import ProfileListAV, ProfileDetailsAV
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('profilelist/', ProfileListAV.as_view()),
     path('profiledetails/<str:pk>/', ProfileDetailsAV.as_view()),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
